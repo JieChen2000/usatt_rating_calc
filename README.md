cf73a5ab676a2ecde89472356af4420ed977d9af
Jay Chen <jie.ch.2000@gmail.com>
2021-06-13 15:28:48 -0500

    Spartans tournament update

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # usatt_rating_calc
 This is a simple calculator to get player's USATT rating change after a tournament. 
+
+
+### Running Enviroment Setup 
+On Mac: 
+git clone https://github.com/JieChen2000/usatt_rating_calc.git
+cd usatt_rating_calc
+mkdir venv 
+cd venv 
+python3 -m venv rating-calc
+cd .. 
+source venv/rating-calc/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
 ### user input 
 User needs put in player name, current rating and a result csv file with format 
 rating,lastname,firstname,winorlose(w/l) which contains win or loss against other players in the tournament. 
-### unit/coverage test 
-coverage run tests/test_rating_calc.py 
-coverage report -m --omit tests/test_rating_calc.py
+
 
 ### run the program 
-python rating_calc.py --player_name Kaye --current_rating 1776 --results_csv hittaopen.csv
+python rating_calc.py --player_name Kaye --current_rating 2034 --results_csv spartansopen.csv
+
+### unit/coverage test for this repo
+coverage run tests/test_rating_calc.py 
+coverage report -m --omit tests/test_rating_calc.py
--- rating_calc.py
+++ rating_calc.py
@@ -22,21 +22,18 @@ if __name__ == '__main__':
         help='Player\'s current rating',
         required=False,
         type=int,
-        default=1551
+        default=2034 
     )
     parser.add_argument(
         '--results_csv',
         help='Player\'s game results for the tournament',
         required=False,
-        default='laopen.csv'
+        default='spartansopen.csv'
     )    
     args = parser.parse_args()
     arguments = args.__dict__    
     A = Player(arguments.pop('player_name'), arguments.pop('current_rating'))
     df_w_l_score = pd.read_csv(arguments.pop('results_csv'))
-
-    # A= player('Hovik M', 1777)
-    # df_w_l_score = pd.read_csv('laopen_hovik.csv')
 
     #%% [markdown]
     # ## output rating changes for the player
@@ -48,15 +45,26 @@ if __name__ == '__main__':
     print("Net rating points gain = ", points_to_add)
     if points_to_add < 50:
         print('Post Tournament Rating = ', points_to_add + A.rating)
-    else:
-        print("Net rating gain > 50, pre tournament rating needs to be adjusted! ")
+    elif points_to_add < 75:
+        print("Net rating gain is between 50~74, pre tournament rating needs to be adjusted with PASS1 method! ")
         ## tier pass1 rating adjustment method, used by USTTA
         print("Adjusted rating with tier pass1 method = ", A.rating + points_to_add)
+        A_adj= Player(A.name, A.rating + points_to_add)
+    #     need implement adjustments for all players against. 
+    #     df_w_l_score = pd.read_csv('laopen_adj.csv')  ##after all the other players' rating being adjusted
+        points_to_add = points_for_games(A_adj, df_w_l_score)    
+        print('Post Tournament Rating = ', points_to_add + A_adj.rating)        
+    else:
+        print("Net rating gain is greater than 75, pre tournament rating needs to be adjusted with PASS2 method! ")
         ## tier pass2 method: NOT USED by USTTA 
         ## Average of pre-tournament rating and average of  best win and worst loss.  
         ## e.g. int((1551+(1781+1608)/2.)/2)) 
-        A_adj= Player(A.name, A.rating + points_to_add)
-    #     need implement adjustments for all players against. 
+        best_win_rating = df_w_l_score[df_w_l_score['winorlose']==' w']['rating'].max()
+        worst_loss_rating = df_w_l_score[df_w_l_score['winorlose']==' l']['rating'].min()
+        print("Adjusted rating with tier pass2 method = ", int((A.rating + (best_win_rating + worst_loss_rating)/2)/2))
+        
+        A_adj= Player(A.name, int((A.rating + (best_win_rating + worst_loss_rating)/2)/2))
+    #     need implement adjustments for all players against. Need all match result. not to do in this app.
     #     df_w_l_score = pd.read_csv('laopen_adj.csv')  ##after all the other players' rating being adjusted
         points_to_add = points_for_games(A_adj, df_w_l_score)    
         print('Post Tournament Rating = ', points_to_add + A_adj.rating)
